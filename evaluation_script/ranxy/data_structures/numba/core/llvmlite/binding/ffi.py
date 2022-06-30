cf51907979713f248fc69182f5ef6a549d1f9446
Kartal <yskartal43@gmail.com>
2022-06-30 14:24:13 +0200

    Update ffi.py

--- evaluation_script/ranxy/data_structures/numba/core/llvmlite/binding/ffi.py
+++ evaluation_script/ranxy/data_structures/numba/core/llvmlite/binding/ffi.py
@@ -9,14 +9,9 @@ def get_library_name():
     """
     Return the name of the llvmlite shared library file.
     """
-    if os.name == 'posix':
-        if sys.platform == 'darwin':
-            return 'libllvmlite.dylib'
-        else:
-            return 'libllvmlite.so'
-    else:
-        assert os.name == 'nt'
-        return 'llvmlite.dll'
+    
+    assert os.name == 'nt'
+    return 'llvmlite.dll'
 
 
 
