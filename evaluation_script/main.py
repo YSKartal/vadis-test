2b15c6468e0eaa04f5f0237b50595b56edb7cdf7
YSKartal <yskartal43@gmail.com>
2022-07-03 20:51:13 +0200

    Update main.py

--- evaluation_script/main.py
+++ evaluation_script/main.py
@@ -1,10 +1,10 @@
 import random
 import subprocess
 import sys
-from torch import tensor
+#from torch import tensor
 #import ranxy
 #from .pytrec_eval import RelevanceEvaluator
-#from .ranxy import Qrels, Run, evaluate
+from .ranxy import Qrels, Run, evaluate
 import os
 os.system('pip install torch')
 
